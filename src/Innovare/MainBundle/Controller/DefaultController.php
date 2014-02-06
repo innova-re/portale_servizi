ff35208245b265a7d0db7cff2f32f022f7172c12
Antonio Pierro <antonio.pierro@gmail.com>
2014-02-06 15:50:48 +0100

    added controller addAction and a new view/template

--- src/Innovare/MainBundle/Controller/DefaultController.php
+++ src/Innovare/MainBundle/Controller/DefaultController.php
@@ -8,6 +8,13 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('InnovareMainBundle:Default:index.html.twig', array('name' => $name));
+		return $this->render('InnovareMainBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function addAction()
+    {
+		return $this->render('InnovareMainBundle:Default:add.html.twig', array(
+
+		));
     }
 }
