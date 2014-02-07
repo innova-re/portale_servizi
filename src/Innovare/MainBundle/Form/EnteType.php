61650470d882c3b8afbeada29e2dba51568b6974
Antonio Pierro <antonio.pierro@gmail.com>
2014-02-07 09:45:19 +0100

    added missing entities

---
+++ src/Innovare/MainBundle/Form/EnteType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Innovare\MainBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EnteType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nome')
+            ->add('descrizione')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Innovare\MainBundle\Entity\Ente'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'innovare_mainbundle_ente';
+    }
+}
---
+++ src/Innovare/MainBundle/Form/SettoreType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Innovare\MainBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class SettoreType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('settore')
+            ->add('settoreTresto')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Innovare\MainBundle\Entity\Settore'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'innovare_mainbundle_settore';
+    }
+}
