7e6ae7a800027d86eabc95d5e824365abdb284fb
Antonio Pierro <antonio.pierro@gmail.com>
2014-02-09 18:15:11 +0100

    reoved useless log

--- src/Innovare/MainBundle/Resources/public/js/main.js
+++ src/Innovare/MainBundle/Resources/public/js/main.js
@@ -6,14 +6,12 @@
 
 		// TODO you shoould find a way to make an ajax request by symfony2
 		var url = window.location.pathname.slice(0, -1);
-		var urlRoot = url.substring(url.lastIndexOf('/'));
 
 		$('.sidebar-nav li').each(function (index, li) {
 			var $li = $(li);
 
 			$li.removeClass('active');
-			if(urlRoot.indexOf(($li.find('a').text()).toLowerCase()) !== -1) {
-				console.log($li.find('a').text())
+			if(url.indexOf($li.find('a').text().toLowerCase()) !== -1) {
 				$li.addClass('active');
 			}
 		});
